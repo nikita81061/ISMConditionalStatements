eee04031749661e38e4bcd0dbc1d71706c9cf909
Nikita <nikita.zaytsev.off@gmail.com>
2019-09-18 10:13:45 +0300

    ISMConditionalStatements

---
+++ WinConsoleTask/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WinConsoleTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z, res;
+            try
+            {
+                Console.WriteLine("Введіть значення x:");
+                x = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Введіть значення y:");
+                y = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Введіть значення z:");
+                z = Double.Parse(Console.ReadLine());
+
+                res = (2 * Math.Cos(x * x) - 0.5) / (0.5 + Math.Sin(Math.Pow(y, 2 - z))) + (z * z / (7 - z / 3));
+                Console.WriteLine("{0}", res);
+            }
+            catch {
+                Console.WriteLine("Помилка введення!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
